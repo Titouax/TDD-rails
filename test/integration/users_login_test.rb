2fad4b2f1829f367d832f645159ec8db2f0616af
TheFSilver <dasylva.f@gmail.com>
2018-08-06 20:38:55 +0200

    Ajout de Tests

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -4,6 +4,7 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
 
   def setup
     @user = users(:michael)
+
   end
 
   test "login with invalid information" do
@@ -24,6 +25,10 @@ class UsersLoginTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", new_user_path, count: 0
     assert_select "a[href=?]", logout_path
     assert_select "a[href=?]", user_path(@user)
+    get user_path(@user)
+
+    assert_select "h3", "#{@user.first_name} #{@user.last_name}"
+    assert_select "p", @user.email
   end
 
 end
