2bc07d5e29733bc089f566f4fdbe786958001770
TheFSilver <dasylva.f@gmail.com>
2018-08-06 19:49:14 +0200

    correct login test

--- test/integration/users_login_test.rb
+++ test/integration/users_login_test.rb
@@ -1,12 +1,12 @@
 require 'test_helper'
 
 class UsersLoginTest < ActionDispatch::IntegrationTest
+
   def setup
-      @user = users(:frank)
-    end
+    @user = users(:michael)
+  end
 
-
-test "login with invalid information" do
+  test "login with invalid information" do
     get login_path
      assert_template 'sessions/new'
     post login_path, params: { session: { email: "", password: "" } }
@@ -17,16 +17,13 @@ test "login with invalid information" do
   end
 
   test "login with valid information" do
-
-  get login_path
-  post login_path, params: { session: { email: @user.email,
-                                        password: @user.password_digest } }
-  assert_redirected_to @user
-  follow_redirect!
-  assert_template 'users/show'
-  assert_select "a[href=?]", login_path, count: 0
-  assert_select "a[href=?]", logout_path
-  assert_select "a[href=?]", user_path(@user)
-end
+    get login_path
+    post login_path, params: { session: { email:    @user.email,
+                                          password: 'password' } }
+    get root_path
+    assert_select "a[href=?]", new_user_path, count: 0
+    assert_select "a[href=?]", logout_path
+    assert_select "a[href=?]", user_path(@user)
+  end
 
 end
